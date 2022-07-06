ad03b58b87e8b1549577c9773aac4ae3d70d4710
Micah Allauca <micah.allauca@gmail.com>
2022-07-06 14:15:19 -0400

    Removed reference to config file in database.js

--- server/database.js
+++ server/database.js
@@ -1,5 +1,4 @@
 const { Sequelize } = require('sequelize');
-const { SEQUELIZE_DB_NAME, SEQUELIZE_USERNAME, SEQUELIZE_PASSWORD } = require('./config');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './database.sqlite'
